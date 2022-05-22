2e7a0151ee1693f24d3be0cee3787d4181dbf078
VArtzy <farrelnikoson@gmail.com>
2022-05-22 20:12:18 +0700

    Memperbaiki perlink-an dan juga beberapa cara curang

--- ujian.js
+++ ujian.js
@@ -1,3 +1,21 @@
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+const sudahMengerjakan = getCookie('nilai')
+
+if(sudahMengerjakan) {
+  alert('Anda sudah mengikuti ujian, jika ingin mengikuti kembali silahkan reset di halaman penilaian')
+  location.href = './penilaian.html'
+}
+
 window.onblur = () => {
     confirm('Anda terkena illegal access!!!')
     location.href = './ilegal.html'
